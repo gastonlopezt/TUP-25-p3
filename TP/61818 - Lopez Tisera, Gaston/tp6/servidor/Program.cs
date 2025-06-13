d2d6fa17a2ecf7fe88156222fe2e0b40022edf9f
Gaston Lopez Tisera <gaston_lopeztisera@hotmail.com>
2025-06-13 16:27:26 -0300

    Metodos Carrito
    creación del get, post y delete

--- TP/61818 - Lopez Tisera, Gaston/tp6/servidor/Program.cs
+++ TP/61818 - Lopez Tisera, Gaston/tp6/servidor/Program.cs
@@ -1,42 +1,16 @@
 using Microsoft.EntityFrameworkCore;
 using Servidor.Datos;
 using Servidor.Modelos;
+using Tienda.Modelos;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<TiendaDbContext>(options =>
     options.UseSqlite("Data Source=tienda.db"));
 
 var app = builder.Build();
+// var testCarrito = new Tienda.Modelos.Carrito();
+var carritos = new Dictionary<Guid, Carrito>();
 
-// using (var scope = app.Services.CreateScope())
-// {
-//     var db = scope.ServiceProvider.GetRequiredService<TiendaDbContext>();
-//     db.Database.EnsureCreated();
-
-//     if (!db.Productos.Any())
-//     {
-//         db.Productos.AddRange(new List<Producto>
-//         {
-//             new() { Nombre = "Celular A1", Descripcion = "Smartphone básico", Precio = 50000, Stock = 20, ImagenUrl = "img/celular1.png" },
-//             new() { Nombre = "Celular B2", Descripcion = "Smartphone gama media", Precio = 75000, Stock = 15, ImagenUrl = "img/celular2.png" },
-//             new() { Nombre = "Celular C3", Descripcion = "Gama alta", Precio = 120000, Stock = 10, ImagenUrl = "img/celular3.png" },
-//             new() { Nombre = "Auriculares X", Descripcion = "Bluetooth", Precio = 15000, Stock = 30, ImagenUrl = "img/auriculares1.png" },
-//             new() { Nombre = "Funda resistente", Descripcion = "Funda para celular", Precio = 5000, Stock = 25, ImagenUrl = "img/funda1.png" },
-//             new() { Nombre = "Cargador rápido", Descripcion = "Cargador USB-C", Precio = 7000, Stock = 20, ImagenUrl = "img/cargador1.png" },
-//             new() { Nombre = "Smartwatch Y", Descripcion = "Reloj inteligente", Precio = 45000, Stock = 12, ImagenUrl = "img/watch1.png" },
-//             new() { Nombre = "Notebook Z", Descripcion = "Laptop liviana", Precio = 220000, Stock = 5, ImagenUrl = "img/laptop1.png" },
-//             new() { Nombre = "Tablet Mini", Descripcion = "Pantalla 8\"", Precio = 90000, Stock = 8, ImagenUrl = "img/tablet1.png" },
-//             new() { Nombre = "Teclado gamer", Descripcion = "RGB mecánico", Precio = 25000, Stock = 14, ImagenUrl = "img/teclado1.png" },
-//         });
-
-//         db.SaveChanges();
-//     }
-// }
-// app.MapGet("/test-productos", async (TiendaDbContext db) =>
-// {
-//     var productos = await db.Productos.ToListAsync();
-//     return Results.Ok(productos);
-// });
 app.MapGet("/productos", async (TiendaDbContext db, string? query) =>
 {
     var productos = string.IsNullOrWhiteSpace(query)
@@ -46,6 +20,28 @@ app.MapGet("/productos", async (TiendaDbContext db, string? query) =>
             .ToListAsync();
 
     return Results.Ok(productos);
+});
+app.MapPost("/carritos", () =>
+{
+    var carrito = new Carrito();
+    carritos[carrito.Id] = carrito;
+    return Results.Ok(carrito.Id);
+});
+
+app.MapGet("/carritos/{id:guid}", (Guid id) =>
+{
+    if (carritos.TryGetValue(id, out var carrito))
+        return Results.Ok(carrito);
+
+    return Results.NotFound();
+});
+
+app.MapDelete("/carritos/{id:guid}", (Guid id) =>
+{
+    if (carritos.Remove(id))
+        return Results.Ok();
+
+    return Results.NotFound();
 });
 
 app.Run();
